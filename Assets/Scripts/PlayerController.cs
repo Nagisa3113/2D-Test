e9ea71332ada58874d5abd2b3b22fb3a6f3c9564
Nagisa3113 <nagisa3113@gmail.com>
2022-04-12 00:14:04 +0800

    add player run animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float MoveSpeed = 10f;
+    public float MoveSpeed = 6f;
+    private Animator _animator;
+    private bool faceRight = true;
+    private SpriteRenderer _spriteRenderer;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Start is called before the first frame updalte
@@ -21,7 +27,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!faceRight)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else
+        {
+            _spriteRenderer.flipX = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
         float h = Input.GetAxis("Horizontal");
+        if (h < 0)
+        {
+            faceRight = false;
+        }
+        else if (h > 0)
+        {
+            faceRight = true;
+        }
+
         rb.velocity = new Vector2(MoveSpeed * h, rb.velocity.y);
+        _animator.SetFloat("Speed", Mathf.Abs(h) * 5f);
     }
 }
